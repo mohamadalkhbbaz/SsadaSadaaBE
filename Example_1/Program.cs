5ba2aa3f8dc300e3490588f2e45d75871d368a9e
mohamed <mohamadalkhbbaz@gmail.com>
2023-12-28 16:37:33 +0300

    ..

---
+++ Example_1/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Example_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Start();
+        }
+        private static void Start()
+        {
+            var stopwatch1 = new Stopwatch();
+            Console.WriteLine("Pls Click Enter To Start");
+            Console.ReadKey();
+            var start = stopwatch1.Start();
+            Console.WriteLine(">>>>>The project is Start<<<<<");
+            Console.WriteLine("Pls Click Enter To Stop And Return Duration Time");
+            Console.ReadKey();
+            var duration = stopwatch1.Stop(start);
+            Console.WriteLine("The Duration Time : " + duration.ToString());
+            Console.WriteLine("Pls Enter start To Do Again");
+
+            var reStart = Console.ReadLine();
+            if (reStart != null && reStart.Length != 0 && reStart.ToString().ToLower() == "start")
+                Start();
+        }
+    }
+}
---
+++ Example_1/Stopwatch.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example_1
+{
+    public class Stopwatch
+    {
+
+        public DateTime Start()
+        {
+            return DateTime.Now;
+        }
+        public TimeSpan Stop(DateTime startTime)
+        {
+           return DateTime.Now - startTime;
+        }
+
+    }
+}
