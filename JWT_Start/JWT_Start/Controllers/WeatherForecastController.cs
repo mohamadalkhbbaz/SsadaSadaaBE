d5aebae5cb3330552894b5bd1522861c35e09505
mohamed <mohamadalkhbbaz@gmail.com>
2023-12-28 16:16:15 +0300

    ..

--- JWT_Start/JWT_Start/Controllers/WeatherForecastController.cs
+++ JWT_Start/JWT_Start/Controllers/WeatherForecastController.cs
@@ -19,7 +19,7 @@ namespace JWT_Start.Controllers
             _logger = logger;
         }
 
-        [HttpGet(Name = "GetWeatherForecast")]
+        [HttpGet(Name = "GetWeatherForecast"),Authorize]
         public IEnumerable<WeatherForecast> Get()
         {
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
